70315930fe1615a2f0d46dc2a8cb1664a2f1a781
AnthonyCost <anthonycost99@gmail.com>
2021-07-28 14:04:17 -0700

    added campaign model

---
+++ app/models/campaign.py
@@ -0,0 +1,17 @@
+from .db import db
+# this needs  a join table related to the campaign and the user
+
+class Campaign(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hostId = db.Column(db.Integer, nullable=False)
+    title = db.Column(db.String(50), nullable=False)
+    coverImage = db.Column(db.String(255), nullable=False)
+    story = db.Column(db.Text, nullable=False)
+
+    def to_dict(self):
+        return {
+        'hostId': self.hostId,
+        'title': self.title,
+        'coverImage': self.coverImage,
+        'story': self.story
+        }
