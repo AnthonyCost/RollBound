3e08eab48a73f2a56b00ff13a84d25f3174f7e8c
AnthonyCost <anthonycost99@gmail.com>
2021-07-30 09:54:11 -0700

    added read to campaigns store

---
+++ react-app/src/store/campaigns.js
@@ -0,0 +1,40 @@
+// Action Creators
+
+const GET_CAMPAIGNS = 'campaigns/GET_CAMPAIGNS';
+
+const getCampaigns = (campaigns) => ({
+    type: GET_CAMPAIGNS,
+    campaigns,
+})
+
+// Thunks
+
+export const grabCampaigns = () => async (dispatch) => {
+    const res = await fetch('/api/campaigns');
+    const campaigns = await res.json();
+    dispatch(getCampaigns(campaigns));
+}
+
+// initial state
+const initialState = {}
+
+// Reducer
+
+const campaignsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_CAMPAIGNS:
+            const allCampaigns = {};
+            action.campaigns.forEach((campaign) => {
+                allCampaigns[campaign.id] = campaign;
+            });
+            return {
+                ...state,
+                ...allCampaigns,
+            };
+            default:
+                return state;
+            }
+        }
+
+// export campaigns reducer
+export default campaignsReducer;
