aaff14b119319c0539baa210dee8a238518c2c8b
AnthonyCost <anthonycost99@gmail.com>
2021-07-30 15:13:14 -0700

    did a little bit moore styling

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
 import campaigns from './campaigns';
+// import character from './characters';
 
 const rootReducer = combineReducers({
   session,
   campaigns
+  // characters
 });
 
 
