73e3d2ebb32e9e7254be681d85c22c22150cf86e
AnthonyCost <anthonycost99@gmail.com>
2021-08-03 15:30:40 -0700

    rendering the characters components

--- react-app/src/components/MyCharactersPage/index.js
+++ react-app/src/components/MyCharactersPage/index.js
@@ -1,36 +1,39 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import CampaignTab from '../CampaignTab';
 import { NavLink } from 'react-router-dom';
-import { grabCampaigns } from '../../store/campaigns';
+import { grabCharacters } from '../../store/characters';
 import "./MyCharacters.css"
 
 const MyCharacters = () => {
 
   const dispatch = useDispatch();
-  const allCampaigns = useSelector(state => Object.values(state.campaigns));
+  const allCharacters = useSelector(state => Object.values(state.characters));
+
+  console.log(allCharacters);
 
   const user = useSelector(state => state.session.user);
   const userId = user?.id
   
-  const campaigns = allCampaigns.filter(campaign => campaign?.hostId?.id === userId);
+  const characters = allCharacters.filter(character => character?.userId?.id === userId);
 
-  
+  console.log(characters);
+
   useEffect(() => {
-    dispatch(grabCampaigns());
+    dispatch(grabCharacters());
   }, [dispatch]);
 
   return (
       <div>
           <div className="index-header">
           <h1>My Characters</h1>
-          <NavLink to={`/campaigns/createCampaign`}>
-                    <button>Add Campaign</button>
+          <NavLink to={`/characters/createCharacter`}>
+                    <button>Add Character</button>
                 </NavLink>
           </div>
           <div className="index-campaignsList">
-              {campaigns?.map((campaign) => (
-                  <CampaignTab key={campaign.id} campaign={campaign} />
+              {characters?.map((character) => (
+                //   <CampaignTab key={campaign.id} campaign={campaign} />
+                <h2>{character?.name}</h2>
               ))}
         </div>
       </div>
--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,12 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
 import campaigns from './campaigns';
-// import character from './characters';
+import characters from './characters';
 
 const rootReducer = combineReducers({
   session,
-  campaigns
-  // characters
+  campaigns,
+  characters
 });
 
 
