029dbe6f65b1654ff611716bd1f54200db768bd5
AnthonyCost <anthonycost99@gmail.com>
2021-08-05 09:43:58 -0700

    404 page fully styled and finished

--- react-app/src/components/PageNotFound/index.js
+++ react-app/src/components/PageNotFound/index.js
@@ -15,22 +15,24 @@ const PageNotFound = () => {
 
   return (
     <div className="pageNotFound-container">
-      <div className="pageNotFound-banner">
-          <div>
-              <img src="https://user-images.githubusercontent.com/35717793/128381842-6474d294-5430-4291-99df-f062f78169ef.png"/>
-          </div>
-          <div className="pageNotFound-header">
-            <h1>Page Not Found</h1>
-          </div>
-        <div className="pageNotFound-subHeader">
-            <h3>Uh oh! Looks like you rolled a 404! (Too bad it wasn't for damage)</h3>
+        <div className="pageNotFound-contentContainer">
+            <div className="pageNotFound-leftSide">
+                <div className="pageNotFound-header">
+                    <h1>Page Not Found</h1>
+                </div>
+                    <div className="pageNotFound-subHeader">
+                        <h3>Uh oh! Looks like you rolled a 404! (Too bad it wasn't for damage)</h3>
+                    </div>
+                    <div className="pageNotFound-btn">
+                        <button type="button" onClick={handleClick}>
+                        Take me back
+                        </button>
+                    </div>
+                </div>
+                <div className="pageNotFound-rightSide">
+                    <img src="https://user-images.githubusercontent.com/35717793/128381842-6474d294-5430-4291-99df-f062f78169ef.png"/>
+                </div>
         </div>
-      </div>
-      <div>
-        <button className="pageNotFound-btn" type="button" onClick={handleClick}>
-          Take me back
-        </button>
-      </div>
     </div>
   );
 };
